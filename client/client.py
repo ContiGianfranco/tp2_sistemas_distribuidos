84d052caa30b51b4efca84e0d48b6e67b0f7cc97
gianfranco <giacocipo@gmail.com>
2022-06-08 14:48:03 -0300

    client now uses Middleware

--- client/client.py
+++ client/client.py
@@ -3,10 +3,10 @@
 import csv
 import json
 import time
-import pika
-import pika.exceptions
 import signal
 import os
+
+from common.middleware import Middleware
 
 NUMBER_OF_ROW = int(os.environ["NUMBER_OF_ROW"])
 AVG_JOINER = int(os.environ["AVG_JOINER"])
@@ -22,30 +22,14 @@ class Client:
         self.all_student_memes = [False] * AVG_JOINER
         self.all_received = [False] * 3
 
-        connected = False
-        while not connected:
-            try:
-                self.connection = pika.BlockingConnection(
-                    pika.ConnectionParameters(host='rabbitmq'))
-                connected = True
-            except pika.exceptions.AMQPConnectionError:
-                print("Rabbitmq not connected yet")
-                time.sleep(1)
-        self.channel = self.connection.channel()
+        self.middleware = Middleware('rabbitmq')
 
-        self.channel.exchange_declare(exchange='result', exchange_type='direct')
-        self.result = self.channel.queue_declare(queue='', durable=True)
-        self.queue_name = self.result.method.queue
+        result_queue = {
+            'exchange': 'result',
+            'keys': ["AVG", "SENT", "STUD"]
+        }
 
-        self.channel.queue_bind(
-            exchange='result', queue=self.queue_name, routing_key="AVG")
-        self.channel.queue_bind(
-            exchange='result', queue=self.queue_name, routing_key="SENT")
-        self.channel.queue_bind(
-            exchange='result', queue=self.queue_name, routing_key="STUD")
-
-        self.channel.queue_declare(queue='posts')
-        self.channel.queue_declare(queue='comments')
+        self.middleware.subscribe(result_queue, self.callback)
 
         signal.signal(signal.SIGTERM, self.stop)
 
@@ -54,11 +38,15 @@ class Client:
         self.stopping = True
 
         if self.consuming:
-            self.channel.stop_consuming()
+            self.middleware.shutdown()
 
     def send_posts(self, file_path):
         file = open(file_path)
         csvreader = csv.reader(file)
+
+        post_queue = {
+            'queue': 'posts'
+        }
 
         header = []
         header = next(csvreader)
@@ -70,18 +58,18 @@ class Client:
 
             if len(rows) >= NUMBER_OF_ROW:
                 data = json.dumps(rows).encode()
-                self.channel.basic_publish(exchange='', routing_key='posts', body=data)
+                self.middleware.publish(post_queue, data)
                 rows = []
                 if self.stopping:
-                    self.connection.close()
+                    self.middleware.close()
                     file.close()
                     exit(0)
 
         if len(rows) > 0:
             data = json.dumps(rows).encode()
-            self.channel.basic_publish(exchange='', routing_key='posts', body=data)
+            self.middleware.publish(post_queue, data)
 
-        self.channel.basic_publish(exchange='', routing_key='posts', body="END".encode())
+        self.middleware.publish(post_queue, "END".encode())
         print("END_OF_POSTS")
         file.close()
 
@@ -89,6 +77,10 @@ class Client:
         file = open(file_path)
         csvreader = csv.reader(file)
 
+        comments_queue = {
+            'queue': 'comments'
+        }
+
         header = []
         header = next(csvreader)
 
@@ -99,30 +91,26 @@ class Client:
 
             if len(rows) >= NUMBER_OF_ROW:
                 data = json.dumps(rows).encode()
-                self.channel.basic_publish(exchange='', routing_key='comments', body=data)
+                self.middleware.publish(comments_queue, data)
                 rows = []
                 if self.stopping:
-                    self.connection.close()
+                    self.middleware.close()
                     file.close()
                     exit(0)
 
         if len(rows) > 0:
             data = json.dumps(rows).encode()
-            self.channel.basic_publish(exchange='', routing_key='comments', body=data)
+            self.middleware.publish(comments_queue, data)
 
-        self.channel.basic_publish(exchange='', routing_key='comments', body="END".encode())
+        self.middleware.publish(comments_queue, "END".encode())
         print("END_OF_COMMENTS")
 
         file.close()
 
-        self.channel.basic_consume(
-            queue=self.queue_name, on_message_callback=self.callback, auto_ack=True)
-
         self.consuming = True
 
-        self.channel.start_consuming()
-        if self.stopping:
-            self.connection.close()
+        self.middleware.wait_for_messages()
+        self.middleware.close()
 
     def callback(self, ch, method, properties, body):
         if method.routing_key == "AVG":
@@ -160,7 +148,7 @@ class Client:
                     self.all_received[2] = True
 
         if False not in self.all_received:
-            self.connection.close()
+            ch.stop_consuming()
 
 
 if __name__ == '__main__':
